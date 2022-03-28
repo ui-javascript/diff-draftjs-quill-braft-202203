ccf91cabee095e40ee80e3698177689dabbaed9d
luo0412 <1095847440@qq.com>
2022-03-28 23:53:00 +0800

    Initial commit

---
+++ __demo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.less';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>首页</h1>
+    </div>
+  );
+};
+export default App;
+
+// ReactDOM.render(<App />, document.getElementById('root'));
---
+++ __demo/page2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+export default () => {
+  return (
+    <div>
+      <h1>Page2</h1>
+    </div>
+  );
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
